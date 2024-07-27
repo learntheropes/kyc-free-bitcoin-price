75955c89ba9a435b01c339989a02051511ff3969
Learntheropes <giovanni@learntheropes.xyz>
2024-07-27 08:20:16 +0200

    fix homepage buttons

--- pages/index.vue
+++ pages/index.vue
@@ -6,25 +6,17 @@
 <template>
   <NuxtLayout>
     <section class="section">
-      <div class="columns is-centered is-multiline is-variable is-16">
-        <div class="buttons">
-          <NuxtLink 
-            v-for="currency in currencies" 
-            :key="currency" 
-            :to="'/'+currency"
-            class="button"
-          >
-            {{ currency }}
-          </NuxtLink>
-        </div>
+      <div class="buttons is-centered">
+        <NuxtLink 
+          v-for="currency in currencies" 
+          :key="currency" 
+          :to="'/'+currency"
+          class="button"
+        >
+          {{ currency }}
+        </NuxtLink>
       </div>
     </section>
     <section class="section has-text-centered">{{ platforms.join(', ') }}</section>
   </NuxtLayout>
 </template>
-
-<style scoped>
-.columns.is-variable.is-16 {
-  --columnGap: 4rem;
-}
-</style>
--- server/api/offers/[currency]/sell/peach.get.js
+++ server/api/offers/[currency]/sell/peach.get.js
@@ -4,21 +4,21 @@ export default defineEventHandler(async event => {
 
   try {
     const currency = getRouterParam(event, 'currency');
+    
     const { price: basePrice } = await ofetch(`https://api.peachbitcoin.com/v1/market/price/BTC${currency}`);
-    const response = await ofetch(`https://api.peachbitcoin.com/v1/offer/search?sortBy=lowestPremium&size=50`, {
+
+    const { offers } = await ofetch(`https://api.peachbitcoin.com/v1/offer/search?sortBy=lowestPremium&size=50`, {
       method: 'POST',
       body: {
         type: "ask"
       }
     });
 
-    const { offers } = response;
-
     let results = [];
 
     offers.forEach(offer => {
       if (offer.meansOfPayment[currency]) {
-        return offer.meansOfPayment[currency].forEach(method => {
+        offer.meansOfPayment[currency].forEach(method => {
           results.push({
             service: 'Peach Bitcoin',
             url: 'https://peachbitcoin.com/referral?code=PR41CA',
