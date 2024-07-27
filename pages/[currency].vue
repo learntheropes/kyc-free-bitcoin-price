c66775d0e0f3554487922de51c5fadbce8a7ab12
Learntheropes <giovanni@learntheropes.xyz>
2024-07-27 10:44:09 +0200

    invert ask and bid

--- pages/[currency].vue
+++ pages/[currency].vue
@@ -44,87 +44,7 @@
         <div class="column is-narrow block">
           <div class="columns is-centered">
             <div class="column is-narrow">
-              <h1 class="title is-4 has-text-centered">BUY (Sell Offers)</h1>
-              <div class="subtitle is-6 has-text-centered">
-                <span class="icon">
-                  <i class="mdi mdi-developer-board" />
-                </span>
-                <NuxtLink
-                  :href="'/api/offers/'+currency+'/sell'"
-                  target="_blank"
-                >/api/offers/{{ currency }}/sell</NuxtLink>
-              </div>
-            </div>
-          </div>
-          <div class="columns is-centered is-mobile">
-            <div class="column is-narrow">
-              <div 
-                v-for="(offer, index) of sellOffers" 
-                :key="index"
-                class="block"
-              >
-                <NuxtLink
-                  :href="offer.url"
-                  target="_blank"
-                >{{ offer.service }}</NuxtLink>
-                <div class="is-hidden-tablet">
-                  <ServiceFeatures :offer="offer" />
-                </div>
-              </div>
-            </div>
-            <div class="column is-narrow is-hidden-mobile">
-              <div 
-                v-for="(offer, index) of sellOffers" 
-                :key="index"
-                class="block"
-              >
-                <ServiceFeatures :offer="offer" />
-              </div>
-            </div>
-            <div class="column is-narrow">
-              <div 
-                v-for="(offer, index) of sellOffers" 
-                :key="index"
-                class="block"
-              >
-                <span>{{ offer.method.replace(/\(([^\)]+)\)/, '') }}</span>
-                <div class="is-hidden-tablet">&nbsp;</div>
-              </div>
-            </div>
-            <div class="column is-narrow">
-              <div 
-                v-for="(offer, index) of sellOffers" 
-                :key="index"
-                class="block"
-              >
-                <span class="has-text-primary has-text-weight-bold">{{ offer.price.toFixed(decimals) }}<span class="is-hidden-tablet">&nbsp;€</span></span> 
-                <span>&nbsp;</span>
-                <span class="has-text-grey is-size-7 is-hidden-mobile">{{ currency }}/BTC</span>
-                <div class="is-hidden-tablet has-text-right">
-                  <span class="is-hidden-tablet has-text-right">+</span>
-                  <span class="has-text-primary is-hidden-tablet has-text-right">{{ ((offer.price - sellOffers[0].price) / sellOffers[0].price * 100).toFixed(2) }}</span>
-                  <span class="is-hidden-tablet has-text-right">%</span>
-                </div>
-              </div>
-            </div>
-            <div class="column is-narrow is-hidden-mobile">
-              <div 
-                v-for="(offer, index) of sellOffers" 
-                :key="index"
-                class="block has-text-right"
-              >
-                <span>+</span>
-                <span class="has-text-primary">{{ ((offer.price - sellOffers[0].price) / sellOffers[0].price * 100).toFixed(2) }}</span>
-                <span>%</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="column is-narrow block">
-          <div class="columns is-centered">
-            <div class="column is-narrow">
-              <h1 class="title is-4 has-text-centered">SELL (Buy Offers)</h1>
+              <h1 class="title is-4 has-text-centered">BID (Buy Offers)</h1>
               <div class="subtitle is-6 has-text-centered">
                 <span class="icon">
                   <i class="mdi mdi-developer-board" />
@@ -195,6 +115,86 @@
               >
                 <span>-</span>
                 <span class="has-text-primary">{{ ((offer.price - buyOffers[0].price) / buyOffers[0].price * 100).toFixed(2) }}</span>
+                <span>%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column is-narrow block">
+          <div class="columns is-centered">
+            <div class="column is-narrow">
+              <h1 class="title is-4 has-text-centered">ASK (Sell Offers)</h1>
+              <div class="subtitle is-6 has-text-centered">
+                <span class="icon">
+                  <i class="mdi mdi-developer-board" />
+                </span>
+                <NuxtLink
+                  :href="'/api/offers/'+currency+'/sell'"
+                  target="_blank"
+                >/api/offers/{{ currency }}/sell</NuxtLink>
+              </div>
+            </div>
+          </div>
+          <div class="columns is-centered is-mobile">
+            <div class="column is-narrow">
+              <div 
+                v-for="(offer, index) of sellOffers" 
+                :key="index"
+                class="block"
+              >
+                <NuxtLink
+                  :href="offer.url"
+                  target="_blank"
+                >{{ offer.service }}</NuxtLink>
+                <div class="is-hidden-tablet">
+                  <ServiceFeatures :offer="offer" />
+                </div>
+              </div>
+            </div>
+            <div class="column is-narrow is-hidden-mobile">
+              <div 
+                v-for="(offer, index) of sellOffers" 
+                :key="index"
+                class="block"
+              >
+                <ServiceFeatures :offer="offer" />
+              </div>
+            </div>
+            <div class="column is-narrow">
+              <div 
+                v-for="(offer, index) of sellOffers" 
+                :key="index"
+                class="block"
+              >
+                <span>{{ offer.method.replace(/\(([^\)]+)\)/, '') }}</span>
+                <div class="is-hidden-tablet">&nbsp;</div>
+              </div>
+            </div>
+            <div class="column is-narrow">
+              <div 
+                v-for="(offer, index) of sellOffers" 
+                :key="index"
+                class="block"
+              >
+                <span class="has-text-primary has-text-weight-bold">{{ offer.price.toFixed(decimals) }}<span class="is-hidden-tablet">&nbsp;€</span></span> 
+                <span>&nbsp;</span>
+                <span class="has-text-grey is-size-7 is-hidden-mobile">{{ currency }}/BTC</span>
+                <div class="is-hidden-tablet has-text-right">
+                  <span class="is-hidden-tablet has-text-right">+</span>
+                  <span class="has-text-primary is-hidden-tablet has-text-right">{{ ((offer.price - sellOffers[0].price) / sellOffers[0].price * 100).toFixed(2) }}</span>
+                  <span class="is-hidden-tablet has-text-right">%</span>
+                </div>
+              </div>
+            </div>
+            <div class="column is-narrow is-hidden-mobile">
+              <div 
+                v-for="(offer, index) of sellOffers" 
+                :key="index"
+                class="block has-text-right"
+              >
+                <span>+</span>
+                <span class="has-text-primary">{{ ((offer.price - sellOffers[0].price) / sellOffers[0].price * 100).toFixed(2) }}</span>
                 <span>%</span>
               </div>
             </div>
