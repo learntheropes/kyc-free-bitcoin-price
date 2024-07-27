b7e1f431ad4ded166f924a5d9a889d57001ffa71
Learntheropes <giovanni@learntheropes.xyz>
2024-07-27 11:38:49 +0200

    handle fetch errors

--- pages/[currency].vue
+++ pages/[currency].vue
@@ -42,6 +42,9 @@
 
 <template>
   <NuxtLayout>
+    <section v-if="buyErrors.length > 0 || sellErrors.length > 0" class="section has-text-centered">
+      Errors: {{ [...buyErrors, ...sellErrors].join(', ') }}
+    </section>
     <section class="section">
       <div class="columns is-centered is-multiline is-variable is-16">
         
@@ -206,9 +209,6 @@
         </div>
 
       </div>
-    </section>
-    <section v-if="buyErrors.length > 0 || sellErrors.length > 0" class="section has-text-centered">
-      Errors: {{ [...buyErrors, ...sellErrors].join(', ') }}
     </section>
   </NuxtLayout>
 </template>
