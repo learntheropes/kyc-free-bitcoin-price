8a39a12741f4ff51dc255674f6cfcf177a3ed614
Learntheropes <giovanni@learntheropes.xyz>
2024-07-31 16:34:05 +0200

    update bisq base url

--- server/api/offers/[currency]/buy/bisq.get.js
+++ server/api/offers/[currency]/buy/bisq.get.js
@@ -1,35 +1,38 @@
-import { ofetch } from 'ofetch';
+import tor from 'tor-request';
 import groupBy from 'lodash.groupby';
 import minBy from 'lodash.minby';
 
 export default defineEventHandler(async event => {
+  const currency = getRouterParam(event, 'currency');
 
-  try {
-    const currency = getRouterParam(event, 'currency');
+  return new Promise((resolve, reject) => {
+    tor.request(`http://runbtcxzz4v2haszypwbrn2baqdo7tlwt6dw7g27cwwaootd4gktwayd.onion/api/offers?market=BTC_${currency}`, function (error, res, body) {
+      if (error) {
+        console.log('bisq buy api error', error);
+        reject({ data: [], error: 'bisq buy' });
+        return;
+      }
 
-    const offers = await ofetch(`https://bisq.markets/api/offers?market=BTC_${currency}`);
-    const buys = offers[`btc_${currency.toLowerCase()}`].buys
-  
-    const methods = groupBy(buys, 'payment_method');
-  
-    const data = Object.keys(methods).reduce((arr, method) => {
-      const offer = parseFloat(minBy(methods[method], 'price').price);
-      const fee = (parseFloat(minBy(methods[method], 'price').price) * 1.15 / 100);
-  
-      arr.push({
-        service: 'Bisq',
-        site: 'https://bisq.network/',
-        features: ['on-chain', 'p2p', 'open-source'],
-        method: capitalize(method.split('_').join(' ')).replace('F2f', 'In Person'),
-        price: parseFloat(offer - fee)
-      });
-  
-      return arr;
-    }, []);
+      const offers = JSON.parse(body);
+      const buys = offers[`btc_${currency.toLowerCase()}`].buys;
 
-    return { data };
-  } catch (error) {
-    console.log('bisq buy api error', error);
-    return { error: 'bisq', data: [] };
-  }
-})
+      const methods = groupBy(buys, 'payment_method');
+      const data = Object.keys(methods).reduce((arr, method) => {
+        const offer = parseFloat(minBy(methods[method], 'price').price);
+        const fee = (parseFloat(minBy(methods[method], 'price').price) * 1.15 / 100);
+
+        arr.push({
+          service: 'Bisq',
+          site: 'https://bisq.network/',
+          features: ['on-chain', 'p2p', 'open-source'],
+          method: capitalize(method.split('_').join(' ')).replace('F2f', 'In Person'),
+          price: parseFloat(offer - fee)
+        });
+
+        return arr;
+      }, []);
+
+      resolve({ data });
+    });
+  });
+});
--- server/api/offers/[currency]/sell/bisq.get.js
+++ server/api/offers/[currency]/sell/bisq.get.js
@@ -1,34 +1,38 @@
-import { ofetch } from 'ofetch';
+import tor from 'tor-request';
 import groupBy from 'lodash.groupby';
 import minBy from 'lodash.minby';
 
 export default defineEventHandler(async event => {
+  const currency = getRouterParam(event, 'currency');
 
-  try {
-    const currency = getRouterParam(event, 'currency');
+  return new Promise((resolve, reject) => {
+    tor.request(`http://runbtcxzz4v2haszypwbrn2baqdo7tlwt6dw7g27cwwaootd4gktwayd.onion/api/offers?market=BTC_${currency}`, function (error, res, body) {
+      if (error) {
+        console.log('bisq buy api error', error);
+        reject({ data: [], error: 'bisq buy' });
+        return;
+      }
 
-    const offers = await ofetch(`https://bisq.markets/api/offers?market=BTC_${currency}`);
-    const sells = offers[`btc_${currency.toLowerCase()}`].sells
+      const offers = JSON.parse(body);
+      const sells = offers[`btc_${currency.toLowerCase()}`].sells;
 
-    const methods = groupBy(sells, 'payment_method');
-  
-    const data = Object.keys(methods).reduce((arr, method) => {
-      const offer = parseFloat(minBy(methods[method], 'price').price);
-      const fee = (parseFloat(minBy(methods[method], 'price').price) * 1.15 / 100);
-  
-      arr.push({
-        service: 'Bisq',
-        site: 'https://bisq.network/',
-        features: ['on-chain', 'p2p', 'open-source'],
-        method: capitalize(method.split('_').join(' ')).replace('F2f', 'In Person'),
-        price: parseFloat(offer + fee)
-      });
-  
-      return arr;
-    }, []);
-    return { data };
-  } catch (error) {
-    console.log('bisq sell api error', error);
-    return { error: 'bisq', data: [] };
-  }
-})
+      const methods = groupBy(sells, 'payment_method');
+      const data = Object.keys(methods).reduce((arr, method) => {
+        const offer = parseFloat(minBy(methods[method], 'price').price);
+        const fee = (parseFloat(minBy(methods[method], 'price').price) * 1.15 / 100);
+
+        arr.push({
+          service: 'Bisq',
+          site: 'https://bisq.network/',
+          features: ['on-chain', 'p2p', 'open-source'],
+          method: capitalize(method.split('_').join(' ')).replace('F2f', 'In Person'),
+          price: parseFloat(offer + fee)
+        });
+
+        return arr;
+      }, []);
+
+      resolve({ data });
+    });
+  });
+});
